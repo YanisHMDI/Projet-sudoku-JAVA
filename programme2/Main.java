7de8e6faae0f831b08d8c3c2d3498e6c014d0f8b
chenouna <chenouna.khalid1031@gmail.com>
2024-05-05 20:33:51 +0200

    healthcare un peu de retard oupsie

--- programme2/Main.java
+++ programme2/Main.java
@@ -6,7 +6,7 @@ public class Main {
             GrilleSudoku grille = SudokuFileIO.chargerGrilleVide(); // Charger une grille vide au démarrage
             SudokuInterface sudokuInterface = new SudokuInterface(grille);
             // Ajout du troisième argument pour le mode (true pour le mode automatique, false pour le mode manuel)
-            SudokuController controller = new SudokuController(grille, sudokuInterface, true);
+            SudokuController controller = new SudokuController(grille, sudokuInterface);
             sudokuInterface.setVisible(true); // Assurez-vous que l'interface est visible
         });
     }
